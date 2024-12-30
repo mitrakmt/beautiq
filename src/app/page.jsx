d46aa1c003773247b6b525a0a57ce3e1364a1eee
Michael Mitrakos <mike.mitrakos@gmail.com>
2024-12-30 18:15:32 +0200

    update

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">  Domain for Sale</h1>
+        <p className="mt-6 text-xl text-neutral-600">Contact us at mike@higglo.io to inquire</p>
+      </main>
+    </>
+  )
+}
